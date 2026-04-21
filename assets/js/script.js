b831da9f7257379b63c188f40fbc78c2da9b6fc8
yechan <yc8569inbiznetcorp.com>
2026-04-21 17:22:51 +0900

    슬라이드 기능 추가 및 디자인 수정

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,29 @@ counters.forEach((counter) => {
 
   update();
 });
+// #region CLIENT SLIDER
+const slides = document.querySelectorAll(".slide");
+const text = document.getElementById("clientText");
+
+if (slides.length > 0) {
+  // 🔥 이거 중요 (에러 방지)
+
+  const messages = [
+    "어린이 돌봄센터 점심 제공",
+    "기업 단체 도시락 납품",
+    "행사 및 스포츠팀 식사 제공",
+    "학교 및 기관 급식 지원",
+  ];
+
+  let current = 0;
+
+  setInterval(() => {
+    slides[current].classList.remove("active");
+
+    current = (current + 1) % slides.length;
+
+    slides[current].classList.add("active");
+    text.innerText = messages[current];
+  }, 2500);
+}
+// #endregion
